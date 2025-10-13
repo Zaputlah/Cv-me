c26ad733c3ce5c9b76b017b336af63b9e58ffeb9
IDStar\IDStar PC User <reza.rr889@gmail.com>
2025-10-13 15:58:57 +0700

    update data

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,18 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
-  // site: 'https://astrofy-template.netlify.app',
-  output: "server", 
-  integrations: [mdx(), sitemap(), tailwind()],
+  output: "server", // tetap "server"
+  adapter: vercel(),
+  site: 'https://mycvblog.vercel.app',
+  integrations: [
+    mdx(),
+    tailwind(),
+    sitemap(),
+  ],
   devOptions: {
     toolbar: false
   }
 });
-
