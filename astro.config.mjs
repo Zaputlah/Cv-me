43fee937735c01bc0c84e4ceb5dbcdf9f448f2bf
IDStar\IDStar PC User <reza.rr889@gmail.com>
2025-10-13 16:08:17 +0700

    update 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://astrofy-template.netlify.app',
-  output: "server", 
+  output: "static", 
   integrations: [mdx(), sitemap(), tailwind()],
   devOptions: {
     toolbar: false
